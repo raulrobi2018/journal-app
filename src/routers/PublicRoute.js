9c81b6dba2df843b305eff021b8acf7012b7dac5
Raul Rodriguez <raulrobi@gmail.com>
2021-03-20 20:56:51 -0300

    Class 248

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,28 +1,28 @@
 import React from "react";
-import {Redirect, Route} from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import PropTypes from "prop-types";
 
 export const PublicRoute = ({
-    // Renombra component a Component porque los componentes se escriben con la primer letra en mayúsculas
-    // El parámetro ...rest es para almacenar el resto de los atributos, como el exact, el path, etc
-    isLoggedIn,
-    component: Component,
-    ...rest
+  // Renombra component a Component porque los componentes se escriben con la primer letra en mayúsculas
+  // El parámetro ...rest es para almacenar el resto de los atributos, como el exact, el path, etc
+  isAuthenticated,
+  component: Component,
+  ...rest
 }) => {
-    //El props en este caso recibiría el history, el location, etc
-    return (
-        <Route
-            {...rest}
-            component={(props) =>
-                // Si NO está autenticado, retorna el componente al cual el usuario quiere entrar y le adiciona
-                //las props, o sea, todo lo que tiene el componente. De lo contrario lo redirige al login
-                !isLoggedIn ? <Component {...props} /> : <Redirect to="/" />
-            }
-        />
-    );
+  //El props en este caso recibiría el history, el location, etc
+  return (
+    <Route
+      {...rest}
+      component={(props) =>
+        // Si NO está autenticado, retorna el componente al cual el usuario quiere entrar y le adiciona
+        //las props, o sea, todo lo que tiene el componente. De lo contrario lo redirige al login
+        isAuthenticated ? <Redirect to="/" /> : <Component {...props} />
+      }
+    />
+  );
 };
 
 PublicRoute.propTypes = {
-    isLoggedIn: PropTypes.bool.isRequired,
-    component: PropTypes.func.isRequired
+  isAuthenticated: PropTypes.bool.isRequired,
+  component: PropTypes.func.isRequired
 };
