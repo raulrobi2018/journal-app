e486db9c6422336bd4a548bdd5ff9ab0d50def4a
Raul Rodriguez <raulrobi@gmail.com>
2021-04-02 21:26:46 -0300

    Class 260 - Display the notes on the menu

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,29 +1,29 @@
 import React from "react";
+import moment from "moment";
 
-export const JournalEntry = () => {
+export const JournalEntry = ({id, date, title, body, url}) => {
+    const noteDate = moment(date);
+
     return (
         <div className="journal__entry pointer">
-            <div
-                className="journal__entry-picture"
-                style={{
-                    backgroundSize: "cover",
-                    backgroundImage:
-                        "url(https://www.gettyimages.es/gi-resources/images/frontdoor/editorial/Velo/GettyImages-Velo-1088643550.jpg)"
-                }}
-            ></div>
+            {url && (
+                <div
+                    className="journal__entry-picture"
+                    style={{
+                        backgroundSize: "cover",
+                        backgroundImage: `url(${url})`
+                    }}
+                ></div>
+            )}
 
             <div className="journal__entry-body">
-                <p className="journal__entry-title">
-                    Camino a una nueva victoria
-                </p>
-                <p className="journal__entry-content">
-                    Se viene otra etapa definitoria
-                </p>
+                <p className="journal__entry-title">{title}</p>
+                <p className="journal__entry-content">{body}</p>
             </div>
 
             <div className="journal__entry-date-box">
-                <span>Monday</span>
-                <h4>10</h4>
+                <span>{noteDate.format("dddd")}</span>
+                <h4>{noteDate.format("Do")}</h4>
             </div>
         </div>
     );
