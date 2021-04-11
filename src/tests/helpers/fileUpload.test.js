ee33d6d33e3046561d558e6e341b7ae0a99d269e
Raul Rodriguez <raulrobi@gmail.com>
2021-04-11 19:50:50 -0300

    Class 280 - Deleting the image from Cloudinary

--- src/tests/helpers/fileUpload.test.js
+++ src/tests/helpers/fileUpload.test.js
@@ -1,6 +1,16 @@
+import cloudinary from "cloudinary";
+
 import {fileUpload} from "../../helpers/fileUpload";
 
+cloudinary.config({
+    cloud_name: "raulrobi",
+    api_key: "792327923169299",
+    api_secret: "ojYLsgNz3dTsbQd8TnRfZ-UCWGM"
+});
+
 describe("Testing fileUpload", () => {
+    // Al pasarle el done estamos diciendo que no se ejecute el test hasta que se llame
+    // al done, el cual es llamado al borrar la imagen
     test("should upload a file and return the url", async () => {
         const resp = await fetch(
             "https://19yw4b240vb03ws8qm25h366-wpengine.netdna-ssl.com/wp-content/uploads/10-Free-To-Use-CORS-Proxies-1024x768.png"
@@ -11,6 +21,14 @@ describe("Testing fileUpload", () => {
         const url = await fileUpload(file);
 
         expect(typeof url).toBe("string");
+
+        //Extrae el id de la imgen asignado por cloudinary para borrar la imagen
+        const segments = url.split("/");
+        const imageId = segments[segments.length - 1].replace(".png", "");
+
+        await cloudinary.v2.api.delete_resources(imageId, {}, () => {
+            // done();
+        });
     });
 
     test("should return an error", async () => {
