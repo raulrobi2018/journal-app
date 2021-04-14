7ad6f737fbbfa0c42832d6dd1361e669cb9e9874
Raul Rodriguez <raulrobi@gmail.com>
2021-04-14 09:10:04 -0300

    Class 281 - Testing startNewNote and its asyncronous actions

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -11,8 +11,26 @@ const firebaseConfig = {
     messagingSenderId: "473744528004",
     appId: "1:473744528004:web:64bac7fa99adfb0d82a8d1"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+const firebaseConfigTesting = {
+    apiKey: "AIzaSyAn2ILd57zAnncZZse79mY7kWRqHgOabx4",
+    authDomain: "journal-app-testing-3d4db.firebaseapp.com",
+    projectId: "journal-app-testing-3d4db",
+    storageBucket: "journal-app-testing-3d4db.appspot.com",
+    messagingSenderId: "370829217024",
+    appId: "1:370829217024:web:cc2c886021a97ef93ff7e5"
+};
+
+//Si estoy en ambiente de testing, configuro la conexión a otra BD
+if (process.env.NODE_ENV === "test") {
+    console.log("testing");
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfigTesting);
+} else {
+    //dev-prod
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+}
 
 console.log("Firebase configurado");
 
--- src/tests/actions/notes.test.js
+++ src/tests/actions/notes.test.js
@@ -1,6 +1,12 @@
+/**
+ * @jest-environment node
+ */
+
 import configureStore from "redux-mock-store"; //ES6 modules
 import thunk from "redux-thunk";
 import {startNewNote} from "../../actions/notes";
+import {db} from "../../firebase/firebase-config";
+import {types} from "../../types/types";
 
 // Configuracion necesaria para probar dispatch
 const middlewares = [thunk];
@@ -13,8 +19,37 @@ const store = mockStore({
 });
 
 describe("Testing notes.js", () => {
+    //Basicamente lo que testea esto es la función startNewNote
     test("should create a new note", async () => {
         // Tiene que ser await porque el startNewNote es asincrona
         await store.dispatch(startNewNote());
+
+        const actions = store.getActions();
+
+        //Luego de grabada la nota se ejecutan los actions activeNote y addNewNoteToList
+        expect(actions[0]).toEqual({
+            type: types.notesActive,
+            payload: {
+                id: expect.any(String),
+                title: "",
+                body: "",
+                date: expect.any(Number),
+                url: ""
+            }
+        });
+
+        expect(actions[1]).toEqual({
+            type: types.notesAddEntry,
+            payload: {
+                id: expect.any(String),
+                title: "",
+                body: "",
+                date: expect.any(Number),
+                url: ""
+            }
+        });
+
+        const docId = actions[0].payload.id;
+        await db.doc(`TESTING/journal/Notes/${docId}`).delete();
     });
 });
