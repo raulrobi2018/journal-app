8075f4244c7769f0f05a1e62ce8ac172422ac087
Raul Rodriguez <raulrobi@gmail.com>
2021-04-14 09:58:55 -0300

    Class 285 - Setting up the environment variables

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,35 +4,20 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCfrF1FhmSgIXuPbCvcbaLyjS6ofLuzlkI",
-    authDomain: "journal-app-e2382.firebaseapp.com",
-    projectId: "journal-app-e2382",
-    storageBucket: "journal-app-e2382.appspot.com",
-    messagingSenderId: "473744528004",
-    appId: "1:473744528004:web:64bac7fa99adfb0d82a8d1"
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID
 };
 
-const firebaseConfigTesting = {
-    apiKey: "AIzaSyAn2ILd57zAnncZZse79mY7kWRqHgOabx4",
-    authDomain: "journal-app-testing-3d4db.firebaseapp.com",
-    projectId: "journal-app-testing-3d4db",
-    storageBucket: "journal-app-testing-3d4db.appspot.com",
-    messagingSenderId: "370829217024",
-    appId: "1:370829217024:web:cc2c886021a97ef93ff7e5"
-};
+console.log(process.env);
 
-//Si estoy en ambiente de testing, configuro la conexión a otra BD
-if (process.env.NODE_ENV === "test") {
-    console.log("testing");
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfigTesting);
-} else {
-    //dev-prod
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-console.log("Firebase configurado");
+console.log("Firebase configurado correctamente");
 
 //Obtengo la referencia a la base de datos
 const db = firebase.firestore();
