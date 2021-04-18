f6f7e6881964960872aa72ad29e4b342a7cb6b88
Raul Rodriguez <raulrobi@gmail.com>
2021-04-18 09:52:11 -0300

    Class 292 - Testing RegisterScreen part 1

---
+++ src/tests/components/auth/RegisterScreen.test.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {RegisterScreen} from "../../../components/auth/RegisterScreen";
+import {mount, configure} from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import {Provider} from "react-redux";
+import {MemoryRouter} from "react-router-dom";
+
+//Configuración necesaria para que funcione mount con React 17
+configure({adapter: new Adapter()});
+import configureStore from "redux-mock-store"; //ES6 modules
+import thunk from "redux-thunk";
+
+import {types} from "../../../types/types";
+
+// jest.mock("../../../actions/auth", () => ({
+//     startRegisterWithEmailPasswordName: jest.fn()
+// }));
+
+// Configuracion necesaria para probar dispatch
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const initState = {
+    auth: {},
+    ui: {
+        loading: false,
+        msgError: null
+    }
+};
+
+let store = mockStore(initState);
+
+const wrapper = mount(
+    <Provider store={store}>
+        <MemoryRouter>
+            <RegisterScreen />
+        </MemoryRouter>
+    </Provider>
+);
+
+describe("Testing RegisterScreen component", () => {
+    test("should display the component correctly", () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test("should return the error when the email field is empty", () => {
+        //Dispara la función handleGoogleLogin
+        const emailField = wrapper.find('input[name="email"]');
+        emailField.simulate("change", {
+            target: {
+                value: "",
+                name: "email"
+            }
+        });
+
+        //Esto es lo mismo que hacerlo con el .prop("onSubmit")
+        wrapper.find("form").simulate("submit", {
+            preventDefault() {}
+        });
+
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual({
+            type: types.uiSetError,
+            payload: "Email is not valid"
+        });
+    });
+
+    // test("should run the startRegisterWithEmailPasswordName with its respectively params", () => {
+    //     //Dispara la función
+    //     wrapper.find("form").prop("onSubmit")({preventDefault() {}});
+
+    //     expect(startRegisterWithEmailPasswordName).toHaveBeenCalledWith(
+    //         "Raul",
+    //         "raulrodriguez@gmail.com",
+    //         "123456",
+    //         "123456"
+    //     );
+    // });
+});
